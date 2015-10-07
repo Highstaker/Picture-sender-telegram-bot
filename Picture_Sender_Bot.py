20bf477a8a0c757ab7e68e8c774aeb5acce71a7a
Highstaker <heights999@yandex.ru>
2015-10-07 12:44:53 +0300

    v0.4
    +Added help button (help text to be written)
    +Added upper limit for picture sending period
    +Fixed a bug that caused the program to freeze when connection was lost
    by wrapping all update-getting and message-sending functions
    +Added logging

--- Picture_Sender_Bot.py
+++ Picture_Sender_Bot.py
@@ -1,31 +1,47 @@
-#!/usr/bin/python3
+#!/usr/bin/python3 -u
+#TODO
+#+Add help
+#+Add upper limit of period
+#+fix freezing on connection loss (the bot won't reconnect even if connection is restored)
 
+import logging
 import telegram
 from os import path, listdir, walk
 from random import choice
 from time import time
 from itertools import chain
+import socket
 
-DEBUG = True
-def debug(*msg):
-	if DEBUG:
-		print('[DEBUG]', msg)
+#if a connection is lost and getUpdates takes too long, an error is raised
+socket.setdefaulttimeout(30)
+
+# logging.basicConfig(level = logging.DEBUG)
+
+logging.basicConfig(format = u'[%(asctime)s] %(filename)s[LINE:%(lineno)d]# %(levelname)-8s  %(message)s', 
+	level = logging.WARNING)
+
 
 ############
 ##PARAMETERS
 ############
 
+HELP_MESSAGE = '''
+Help message.
+'''
+
 with open(path.join(path.dirname(path.realpath(__file__)), 'token'),'r') as f:
 	BOT_TOKEN = f.read().replace("\n","")
 
-KEY_MARKUP = [["/subscribe","/unsubscribe","/gimmePic"]]
+KEY_MARKUP = [["/subscribe","/unsubscribe","/gimmePic","/help"]]
 
 FOLDER = path.join(path.expanduser("~"), 'pic_bot_pics')
 
-#A minimum picture sending period a user can set
-MIN_PICTURE_SEND_PERIOD = 5
+#A minimum and maximum picture sending period a user can set
+MIN_PICTURE_SEND_PERIOD = 30
+MAX_PICTURE_SEND_PERIOD = 86400
 
-PICTURE_SEND_PERIOD = 60 * 5
+#A default send period
+PICTURE_SEND_PERIOD = 600
 # PICTURE_SEND_PERIOD = 5#debug
 
 ################
@@ -61,42 +77,75 @@ class TelegramBot():
 		self.files = get_filepaths_incl_subfolders(FOLDER)
 
 	def sendMessage(self,chat_id,text):
-		self.bot.sendMessage(chat_id=chat_id,
-						text=text,
-						reply_markup=telegram.ReplyKeyboardMarkup(KEY_MARKUP)
-						)
+		logging.warning("Replying to " + str(chat_id) + " " + text)
+		while True:
+			try:
+				self.bot.sendMessage(chat_id=chat_id,
+					text=text,
+					reply_markup=telegram.ReplyKeyboardMarkup(KEY_MARKUP)
+					)
+			except telegram.error.TelegramError:
+				logging.error("Could not send message. Retrying!")
+				continue
+			break
+
+	def sendPic(self,chat_id,pic):
+		logging.warning("Sending image to " + str(chat_id) + " " + str(pic))
+		while True:
+			try:
+				logging.debug("Picture: " + str(pic))
+				#set file read cursor to the beginning. This ensures that if a file needs to be re-read (may happen due to exception), it is read from the beginning.
+				pic.seek(0)
+				self.bot.sendPhoto(chat_id=chat_id,photo=pic)
+			except telegram.error.TelegramError as e:
+				logging.error("Could not send picture. Retrying! Error: " + str(e))
+				continue
+			break
 
 	def sendRandomPic(self,chat_id):
 		with open( choice( self.files ),"rb" ) as pic:
-			self.bot.sendPhoto(chat_id=chat_id,photo=pic)
+			logging.warning("Sending image to " + str(chat_id) + " " + str(pic))
+			self.sendPic(chat_id,pic)
+
 
 	def echo(self):
 		bot = self.bot
 
 		for i in self.subscribers:
-			debug('sending images')
-
 			if (time() - self.subscribers[i][1]) > self.subscribers[i][0]:
 				self.sendRandomPic(i)
 				self.subscribers[i][1] = time()
 
-		updates = bot.getUpdates(offset=self.LAST_UPDATE_ID, timeout=3)
+		#if getting updates fails - retry
+		while True:
+			try:
+				updates = bot.getUpdates(offset=self.LAST_UPDATE_ID, timeout=3)
+			except telegram.error.TelegramError:
+				logging.error("Could not read updates. Retrying!")
+				continue
+			break
 
 		for update in updates:
 			chat_id = update.message.chat_id
 			Message = update.message
+			from_user = Message.from_user
 			message = Message.text
+			logging.warning("Received message: " + str(chat_id) + " " + from_user.username + " " + message)
 
 			if message == "/subscribe":
 				if not chat_id in self.subscribers:
 					self.subscribers[chat_id] = [PICTURE_SEND_PERIOD,time()]
 					self.sendMessage(chat_id=chat_id,
-						text="You're now subscribed. To cancel subscription enter /unsubscribe",
+						text="You're now subscribed. To cancel subscription enter /unsubscribe. To change the period of picture sending type a number.",
 						)
 				else:
 					self.sendMessage(chat_id=chat_id,
-						text="You have already subscribed. To cancel subscription enter /unsubscribe",
+						text="You have already subscribed. To cancel subscription enter /unsubscribe. To change the period of picture sending type a number.",
 						)
+			elif message == "/help":
+				self.sendMessage(chat_id=chat_id
+					,text=HELP_MESSAGE
+					)
 			elif message == "/unsubscribe":
 				try:
 					del self.subscribers[chat_id]
@@ -117,6 +166,12 @@ class TelegramBot():
 						self.subscribers[chat_id][0] = MIN_PICTURE_SEND_PERIOD
 						self.sendMessage(chat_id=chat_id,
 							text="The minimum possible period is " + str(MIN_PICTURE_SEND_PERIOD) + ".\nSetting period to " + str(MIN_PICTURE_SEND_PERIOD) + "."
+							)
+						self.subscribers[chat_id][1] = time()
+					elif new_period > MAX_PICTURE_SEND_PERIOD:
+						self.subscribers[chat_id][0] = MAX_PICTURE_SEND_PERIOD
+						self.sendMessage(chat_id=chat_id,
+							text="The maximum possible period is " + str(MAX_PICTURE_SEND_PERIOD) + ".\nSetting period to " + str(MAX_PICTURE_SEND_PERIOD) + "."
 							)
 						self.subscribers[chat_id][1] = time()
 					else:
--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 #Random Picture Sender bot for Telegram
 
-This bot sends a random picture from a specified folder (subfolders included) to a subscribed user once in a fixed period of time. A user can change the period by typing a number.
+##Overview
 
-Additionally, a user may request a random picture right away.
+This bot sends a random picture from a specified folder (subfolders included) to a subscribed user once in a fixed period of time. A user can change the period by typing a number. Additionally, a user may request a random picture right away.
+
+##Deployment
 
 To use this program create a file called "token" in the script's folder and paste your bot's token into it. Then change the FOLDER variable to represent the path to your picture folder.
+
+##Dependencies
+
+This program relies on python-telegram-bot.
+https://github.com/leandrotoledo/python-telegram-bot
+
+To install it, use:
+pip install python-telegram-bot
